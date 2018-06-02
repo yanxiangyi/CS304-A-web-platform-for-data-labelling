98329d192203a949a46e32d4870174f0f5f69179
Tiankai-Jiang <11510109@mail.sustc.edu.cn>
2018-06-02 19:00:40 +0800

    a

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -94,24 +94,32 @@ jQuery(document).ready(function () {
     var user_email = "";
     $.ajax({
         type: 'GET',
-        url: 'http://47.106.34.103:5000/email_current',
+        url: 'http://47.106.34.103:5000/profile',
         success: function (json) {
-            user_email = json.message
+            // user_email = json.message
             // user_email='jiangtk@sb.com';
-            $.ajax({
-                type: 'GET',
-                url: 'http://47.106.34.103:5000/profile/' + user_email,
-                success: function (json) {
-                    var parsedData = json.message;
-                    document.getElementById('usrname').innerHTML = parsedData.user_name;
-                    document.getElementById('inner_usrname').innerHTML = parsedData.user_name;
-                    document.getElementById('inner_email').innerHTML = parsedData.user_email;
-                    document.getElementById('figure1').innerHTML = parsedData.num_val;
-                    document.getElementById('figure2').innerHTML = parsedData.num_val_tp;
-                    document.getElementById('figure3').innerHTML = parsedData.num_acc;
-                    document.getElementById('figure4').innerHTML = parsedData.user_credit;
-                }
-            });
+            var parsedData = json.message;
+            document.getElementById('usrname').innerHTML = parsedData.user_name;
+            document.getElementById('inner_usrname').innerHTML = parsedData.user_name;
+            document.getElementById('inner_email').innerHTML = parsedData.user_email;
+            document.getElementById('figure1').innerHTML = parsedData.num_val;
+            document.getElementById('figure2').innerHTML = parsedData.num_val_tp;
+            document.getElementById('figure3').innerHTML = parsedData.num_acc;
+            document.getElementById('figure4').innerHTML = parsedData.user_credit;
+            // $.ajax({
+            //     type: 'GET',
+            //     url: 'http://47.106.34.103:5000/profile/' + user_email,
+            //     success: function (json) {
+            //         var parsedData = json.message;
+            //         document.getElementById('usrname').innerHTML = parsedData.user_name;
+            //         document.getElementById('inner_usrname').innerHTML = parsedData.user_name;
+            //         document.getElementById('inner_email').innerHTML = parsedData.user_email;
+            //         document.getElementById('figure1').innerHTML = parsedData.num_val;
+            //         document.getElementById('figure2').innerHTML = parsedData.num_val_tp;
+            //         document.getElementById('figure3').innerHTML = parsedData.num_acc;
+            //         document.getElementById('figure4').innerHTML = parsedData.user_credit;
+            //     }
+            // });
         }
     });
     $.ajax({
