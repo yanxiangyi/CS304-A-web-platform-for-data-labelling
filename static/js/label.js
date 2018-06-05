d099dd4b328b747fd3080100be6b7292d018ee17
Tiankai-Jiang <11510109@mail.sustc.edu.cn>
2018-06-05 11:38:54 +0800

    debug

--- static/js/label.js
+++ static/js/label.js
@@ -1,4 +1,4 @@
-var json_to_return = undefined;
+// var json_to_return = undefined;
 jQuery(document).ready(function() {
     // var json_to_return;
     // var ds_name;
@@ -6,12 +6,10 @@ jQuery(document).ready(function() {
         type: 'GET',
         url: 'http://47.106.34.103:5000/data',
         success: function (json){
-            json_to_return = json;
-            console.log(json);
-            console.log(json_to_return);
-            console.log(typeof (json));
-            console.log(typeof(json_to_return));
-            ds_name = json.message.projectName;
+            document.getElementById("jsonstring").setAttribute("name", JSON.stringify(json));
+            document.getElementById("projname").setAttribute("name", json.message.projectName);
+            // json_to_return = JSON.stringify(json);
+            // ds_name = json.message.projectName;
             for (let i = 0; i < json.message.length; i++){
             // json_to_return = json;
             jsonObject = json.message;
@@ -163,52 +161,54 @@ jQuery(document).ready(function() {
         }
         }
     });
+});
 
-    function gatherValues() {
-        for (let i = 0; i < json_to_return.message.length; i++){
-            for(let j = 0; j<json_to_return.message[i].task.length; j++){
-                if(json_to_return.message[i].task[j].mode === "single"){
-                    var radioname = "radioWrapper" + i + j;
-                    if ($("input[name=" + radioname + "]:checked").length > 0){
-                        json_to_return.message[i].task[j].label = document.querySelector('input[name="' + radioname + '"]:checked').value;
-                    }
-                    // var radios = document.getElementsByName("radioWrapper" + i + j);
-                    // for (let k = 0; k < radios.length; k++){
-                    //     if (radios[i].checked){
-                    //         json_to_return.message[i].task[j].label = radios[i].value;
-                    //         break;
-                    //     }
-                    // }
-                }else if(json_to_return.message[i].task[j].mode === "multiple"){
-                    var checkboxname = "checkboxWrapper" + i + j;
-                    var returnArray = $("input:checkbox[name=" + checkboxname + "]:checked").map(function(){return $(this).val()}).get();
-                    // json_to_return.message[i].task[j].label = document.querySelector('input[name="' + checkboxname + '"]:checked').value;
-                    json_to_return.message[i].task[j].label = returnArray;
-                }else if(json_to_return.message[i].task[j].mode === "open"){
-                    var selectedname = "#selectedwrapper" + i + j;
-                    json_to_return.message[i].task[j].label = $(selectedname).val();
+function gatherValues() {
+    json_to_return = JSON.parse(document.getElementById("jsonstring").name);
+    for (let i = 0; i < json_to_return.message.length; i++){
+        for(let j = 0; j<json_to_return.message[i].task.length; j++){
+            if(json_to_return.message[i].task[j].mode === "single"){
+                var radioname = "radioWrapper" + i + j;
+                if ($("input[name=" + radioname + "]:checked").length > 0){
+                    json_to_return.message[i].task[j].label = document.querySelector('input[name="' + radioname + '"]:checked').value;
                 }
+                // var radios = document.getElementsByName("radioWrapper" + i + j);
+                // for (let k = 0; k < radios.length; k++){
+                //     if (radios[i].checked){
+                //         json_to_return.message[i].task[j].label = radios[i].value;
+                //         break;
+                //     }
+                // }
+            }else if(json_to_return.message[i].task[j].mode === "multiple"){
+                var checkboxname = "checkboxWrapper" + i + j;
+                var returnArray = $("input:checkbox[name=" + checkboxname + "]:checked").map(function(){return $(this).val()}).get();
+                // json_to_return.message[i].task[j].label = document.querySelector('input[name="' + checkboxname + '"]:checked').value;
+                json_to_return.message[i].task[j].label = returnArray;
+            }else if(json_to_return.message[i].task[j].mode === "open"){
+                var selectedname = "#selectedwrapper" + i + j;
+                json_to_return.message[i].task[j].label = $(selectedname).val();
             }
         }
-        alert(json_to_return.message[0].task[1].label);
-        // $.ajax({
-        //     type: 'POST',
-        //     url: 'http://47.106.34.103:5000/retrieve',
-        //     data:  JSON.stringify (json_to_return), //'{"name":"jonas"}',
-        //     success: function(data) {
-        //         alert("Thank you!");
-        //         window.location.href = "choose.html";
-        //     },
-        //     contentType: "application/json",
-        //     dataType: 'json'
-        // });
     }
-    function queryAgain(){
-        fetch_address = "http://47.106.34.103:5000/choose/" + ds_name;
-        window.location.href = fetch_address;
-    }
-    $('button#submit_result').on('click', gatherValues())
-    $('button#five_more').on('click', gatherValues(), queryAgain());
+    alert(json_to_return.message[0].task[1].label);
+    // $.ajax({
+    //     type: 'POST',
+    //     url: 'http://47.106.34.103:5000/retrieve',
+    //     data:  JSON.stringify (json_to_return), //'{"name":"jonas"}',
+    //     success: function(data) {
+    //         alert("Thank you!");
+    //         window.location.href = "choose.html";
+    //     },
+    //     contentType: "application/json",
+    //     dataType: 'json'
+    // });
+}
+function queryAgain(){
+    ds_name = document.getElementById("projname").name;
+    fetch_address = "http://47.106.34.103:5000/choose/" + ds_name;
+    window.location.href = fetch_address;
+}
+$('button#submit_result').on('click', gatherValues())
+$('button#five_more').on('click', gatherValues(), queryAgain());
 //     $( "#submit_result" ).click(gatherValues());
 //     $( "#five_more" ).click(gatherValues(), queryAgain());
-});
