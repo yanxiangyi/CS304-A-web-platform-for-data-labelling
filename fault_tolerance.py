3b8ad2b1d0552ec616edcd0fe9c4dd9f8bc71ca3
Trevor Xu <10319343@qq.com>
2018-06-11 14:24:06 +0800

    final change of fault_t

--- fault_tolerance.py
+++ fault_tolerance.py
@@ -1,18 +1,16 @@
-import database
+#coding=utf-8
+
 
 def fault_tolerance(ans):
     total = 0
     answers = []
     accset = []
-    # conn = connection.MySQLConnection(user='root',
-    #                                  password='se2018',
-    #                                  host='127.0.0.1',
-    #                                  database='se_proj')
-    # a = database.sql_conn(conn)
-    # ans = a.load_ft_data()
     if len(ans)>1 :
+        # print('length: ')
+        # print(len(ans))
         for i in ans:
             if i[1] not in answers :
+                # print(i[1])
                 if i[4] < 10:
                     answers.append(i[1])
                     accset.append(0.5)
@@ -30,11 +28,17 @@ def fault_tolerance(ans):
                     total += i[3]/i[4]
         for k in accset:
             if k/total >= 0.8:
-                coranswer = k
+                coranswer = answers[accset.index(k)]
                 answerset = []
                 for an in ans:
                     if an[1] == coranswer:
                         answerset.append(an[0])
+                # print(total)
+                # print(k/total)
                 return answerset
     else :
         return None
+#
+# if __name__ == '__main__':
+#     ans= [(81, '1', 1, 15, 15),(66, '1', 2, 2, 3),(60, '1', 3, 0, 0),(65, '1', 17, 0, 0),(70, '1', 18, 0, 0),(76, '1', 19, 0, 5),(73, '[``, [`开心`, `满意`], []]', 26, 0, 0)]
+#     print(fault_tolerance(ans))
